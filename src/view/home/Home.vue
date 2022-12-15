328f0d3ae2ae246c302043cf306bb4fa47f65155
liuhaitao <you@example.com>
2022-12-15 16:29:49 +0800

    头部，轮播图，axios请求了数据

--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -1,14 +1,48 @@
 <template>
-<h2>我是首页内容</h2>
+    <div class="home">
+        <nav-bar class="home-nav">
+            <div slot="center">购物街</div>
+        </nav-bar>
+
+
+        <home-swiper :banners="banners" />
+    </div>
 
 </template>
 
 <script>
+import NavBar from '@/components/common/navbar/NavBar.vue';
+import HomeSwiper from './childComps/HomeSwiper.vue'
+
+import { getHomeMultidata } from '../../network/home'
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        NavBar,
+        HomeSwiper
+
+    },
+
+    data() {
+        return {
+            banners: [],
+            recommends: []
+        }
+    },
+    created() {
+        getHomeMultidata().then(res => {
+            this.banners = res.data.banner.list
+            this.recommends = res.data.recommend.list
+        })
+    }
 }
+
 </script>
 
-<style>
-
+<style scoped>
+.home-nav {
+    background: var(--color-tint);
+    color: #fff;
+    font-size: 25px;
+}
 </style>
