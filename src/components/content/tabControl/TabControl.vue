1f4934c0e6aae013e283c30c54d02747d6a73664
liuhaitao <you@example.com>
2022-12-16 19:48:26 +0800

    请求商品详情的数据

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+    <!-- home页面中的流行新款商品栏 -->
+    <div class="tab-control">
+        <!-- 遍历home页面传过来的titles-->
+        <div v-for="(item, index) in titles" :key="index" class="tab-control-item" @click="itemclick(index)">
+            <!-- 在data中设置currenindex和index的索引值一样便于点击后的颜色 -->
+            <span :class="{ active: currenindex == index }">{{ item }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabControl',
+    props: {
+        titles: {
+            type: Array,
+            default: {
+                return: []
+            }
+        }
+    },
+    data() {
+        return {
+            currenindex: 0
+        }
+    },
+    methods: {
+        itemclick(index) {
+            //给上面绑定点击事件和遍历的index值一样
+            this.currenindex = index
+
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.tab-control {
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+    background: #fff;
+    z-index: 9;
+}
+
+.tab-control-item {
+    flex: 1;
+}
+
+.tab-control-item span {
+    padding: 5px;
+}
+
+.tab-control-item span.active {
+    border-bottom: 5px solid var(--color-tint);
+    color: var(--color-tint);
+}
+</style>
--- src/view/home/Home.vue
+++ src/view/home/Home.vue
@@ -4,36 +4,75 @@
             <div slot="center">购物街</div>
         </nav-bar>
         <home-swiper :banners="banners" />
-        <recommend-view :recommends="recommends"/>
+        <recommend-view :recommends="recommends" />
+        <feature-view />
+        <tab-control :titles="['流行', '新款', '精选']" class="tab-control" />
+
+
     </div>
 
 </template>
 
 <script>
+import TabControl from '@/components/content/tabControl/TabControl.vue'
 import NavBar from '@/components/common/navbar/NavBar.vue';
+
+
 import HomeSwiper from './childComps/HomeSwiper.vue'
 import RecommendView from './childComps/RecommendView.vue'
-import { getHomeMultidata } from '../../network/home'
+import FeatureView from './childComps/FeatureView.vue';
+
+import { getHomeMultidata, getHomeGoods } from '../../network/home'
 export default {
     name: 'Home',
     components: {
+        TabControl,
         NavBar,
         HomeSwiper,
-        RecommendView
+        RecommendView,
+        FeatureView,
+
 
     },
 
     data() {
         return {
             banners: [],
-            recommends: []
+            recommends: [],
+            goods: {
+                'pop': { page: 0, list: [] },
+                'new': { page: 0, list: [] },
+                'sell': { page: 0, list: [] },
+            },
         }
     },
     created() {
-        getHomeMultidata().then(res => {
-            this.banners = res.data.banner.list
-            this.recommends = res.data.recommend.list
-        })
+        this.getHomeMultidata()
+        // 请求'流行', '新款', '精选'三个的数据
+        // 2.请求商品数据
+        this.getHomeGoods('pop');
+        this.getHomeGoods('new');
+        this.getHomeGoods('sell');
+
+
+    },
+    methods: {
+        // 轮播图的请求 
+        getHomeMultidata() {
+            getHomeMultidata().then(res => {
+                this.banners = res.data.banner.list
+                this.recommends = res.data.recommend.list
+            })
+        },
+        getHomeGoods(type) {
+            const page = this.goods[type].page + 1;
+            getHomeGoods(type, page).then(res => {
+                // push不能直接传入一个数组，会被当成一个元素传入，应该使用遍历push或es6方法
+                this.goods[type].list.push(...res.data.list);
+                this.goods[type].page += 1;
+           
+            })
+        }
     }
 }
 
@@ -44,5 +83,10 @@ export default {
     background: var(--color-tint);
     color: #fff;
     font-size: 25px;
+}
+
+.tab-control {
+    position: sticky;
+    top: 40px;
 }
 </style>
